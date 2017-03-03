2cb5bc1593c2c33cc21fede9459f78e7174af29b
Calvin Ference <cference@inocybe.ca>
2017-03-03 12:59:31 -0500

    WIP - Interpreter

--- src/main.go
+++ src/main.go
@@ -2,27 +2,74 @@ package main
 
 import (
 	"fmt"
-	"github.com/vezril/go-cookiejar/collections/stack"
+	"io/ioutil"
+	"github.com/vezril/go-cookiejar/collections/tape"
 )
 
-func addToStack(s *stack.Stack, m int, n int) {
-    for i := m; i < n; i++ {
-        s.Push(i)
-    }
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type Interpreter struct {
+	// Instructions
+	i *tape.Tape
+	// Data
+	d *tape.Tape
+}
+
+func New() *Interpreter {
+	interpreter := new(Interpreter)
+	interpreter.i = tape.New()
+	interpreter.d = tape.New()
+	return interpreter
 }
 
 func main() {
 
-    s := stack.New()
+		dat, err := ioutil.ReadFile("/home/cference/.go/src/github.com/vezril/golang-brainfuck/src/hello.bf")
+		check(err)
+		interpreter := New()
 
-    for i := 0; i < 5; i++ {
-        go addToStack(s, 0+i, 5*i)
-    }
+		// Load instruction data into memory
 
-    fmt.Println(s)
+		for _,b := range dat {
+			interpreter.i.Write(b)
+			interpreter.i.Right()
+		}
+		interpreter.i.Sync(0)
+		signal := true
 
-    // Pop out the stack contents and display them
-    for !s.Empty() {
-        fmt.Println(s.Pop())
-    }
+		for signal {
+				token := interpreter.i.Read()
+				if token == 62 { // >
+					interpreter.d.Right()
+				} else if token == 60 { // <
+					interpreter.d.Left()
+				} else if token == 43 { // +
+					interpreter.d.Inc()
+				} else if token == 45 { // -
+					interpreter.d.Dec()
+				} else if token == 46 { // .
+					fmt.Println(string(interpreter.d.Read()))
+				} else if token == 91 { // [
+					if interpreter.d.Read() == 0 {
+						interpreter.i.SyncToClosing()
+					}
+				} else if token == 93 { // ]
+					if interpreter.d.Read() != 0 {
+						interpreter.i.SyncToOpening()
+						// This is so the catch all instruction pointer gets set at the correct position.
+						// I should probably refactor this to something less ugly. switch-case maybe?
+						interpreter.i.Left()
+					}
+				} else if token == 0 {
+					signal = false
+				}
+				// Increment instruction pointer
+				interpreter.i.Right()
+		}
+		//
+
 }
